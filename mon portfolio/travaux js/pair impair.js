3c723d2a836c7128d6a1b9f7754bf6e16a67faa5
feng <danny.junior484@gmail.com>
2022-06-10 15:46:43 +0200

    portfolio

---
+++ mon portfolio/travaux js/pair impair.js
@@ -0,0 +1,11 @@
+let nb ;
+let choix ="oui"
+while(choix==oui){
+nb=prompt("donnez un nombre")};
+if (nb%2=1){
+    console.log("le nbre est impair");
+}
+else {
+    console.log("le nbre est pair");
+}
+choix=prompt("voulez-vous recommencer?");
---
+++ mon portfolio/travaux js/trico.js
@@ -0,0 +1,9 @@
+let couleur= prompt ("quelle est la couleur ?");
+
+if (couleur=="rouge") {
+    console.log("s'arreter");
+}else if (couleur=="orange") {
+    console.log("ralentir");
+}else if (couleur=="vert"){
+    console.log("passer");
+}
